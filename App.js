312c953cb86780c81ca68bf5ae4ddac2827b528e
IntoKanto <anatoli.tselms@myy.haaga-helia.fi>
2022-09-07 21:03:43 +0300

    Ostoslista

--- App.js
+++ App.js
@@ -1,10 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+
+  const [syote, setSyote] = useState('');
+  const [data, setData] = useState([]);
+ 
+
+  const add = () => {
+    const lisa = syote;
+    setData([...data, {key: lisa}]);
+    setSyote('');
+  }
+
+  const clear = () => {
+    setData([]);
+  }
+
+ 
+ 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.syotekentta}>
+        <TextInput 
+        style={styles.input}
+        value={syote}
+        onChangeText={text => setSyote(text)}
+        />
+        
+        <View style={styles.buttons}>
+          <Button
+          title='Add'
+          onPress={add}
+          />
+          <Button 
+          title='Clear'
+          onPress={clear}
+          />
+        </View>
+     
+      </View>
+
+      <View style={styles.lista}>
+      <Text style={styles.teksti}>Shopping List</Text>
+      <FlatList
+        
+        data={data}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+        keyExtractor={(item, index) => index.toString()}
+      />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +64,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  syotekentta: {
+    flex :1,
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+
+  },
+  buttons : {
+    
+    alignItems: 'baseline',
+    flexDirection: 'row'
+  },
+  input :{
+    width: 200,
+    borderColor: 'blue',
+    borderWidth: 1
+  },
+  lista: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  teksti: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 25
+  }
 });
